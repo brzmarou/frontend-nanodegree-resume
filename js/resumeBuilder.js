893c9f3ace80da2bec612c8f3702294731ff6486
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2016-12-23 22:28:13 +0100

    add - work obj.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,18 +12,32 @@ var bio = {
 	"skills": ["programowanie", "Pozycjonowanie stron", "wordpress"]
 };
 
+var cen = {
+	"workEmployer" : "Centrum Edukacji Nauczycieli w Gdańsku",
+	"workTitle" : "Specjalista ds. e-learningu",
+	"workDates" : "2014-2015",
+	"workLocation" : "Gdańsk, Polska",
+	"workDescription" : "administracja systemami, tworzenie e-learningu, programowanie"
+}
+
+var gdk = {
+	"workEmployer" : "Gdański Dom Książki w Gdańsku",
+	"workTitle" : "informatyk",
+	"workDates" : "2013",
+	"workLocation" : "Gdańsk, Polska",
+	"workDescription" : "administracja systemami, e-marketing"
+}
+
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
-
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
 var formattedContactInfo = HTMLemail.replace("%data%", bio.contactInfo);
-
 var formattedPictureURL = HTMLbioPic.replace("%data%", bio.pictureURL);
-
 var formattedWelcomeMassage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMassage);
 
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-
+var formattedSkills1 = HTMLskills.replace("%data%", bio.skills[0]);
+var formattedSkills2 = HTMLskills.replace("%data%", bio.skills[1]);
+var formattedSkills3 = HTMLskills.replace("%data%", bio.skills[2]);
+var formattedSkills4 = HTMLskills.replace("%data%", bio.skills[3]);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
@@ -31,4 +45,26 @@ $("#footerContacts").prepend(formattedContactInfo);
 $("#header").append(formattedPictureURL);
 $("#header").append(formattedWelcomeMassage);
 $("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills);
+$("#header").append(formattedSkills1);
+$("#header").append(formattedSkills2);
+$("#header").append(formattedSkills3);
+$("#header").append(formattedSkills4);
+
+$("#workExperience").append(HTMLworkStart);
+$("#workExperience").append(HTMLworkEmployer.replace("%data%", cen.workEmployer));
+$("#workExperience").append(HTMLworkTitle.replace("%data%", cen.workTitle));
+$("#workExperience").append(HTMLworkDates.replace("%data%", cen.workDates));
+$("#workExperience").append(HTMLworkLocation.replace("%data%", cen.workLocation));
+$("#workExperience").append(HTMLworkDescription.replace("%data%", cen.workDescription));
+
+$("#workExperience").append(HTMLworkStart);
+$("#workExperience").append(HTMLworkEmployer.replace("%data%", gdk.workEmployer));
+$("#workExperience").append(HTMLworkTitle.replace("%data%", gdk.workTitle));
+$("#workExperience").append(HTMLworkDates.replace("%data%", gdk.workDates));
+$("#workExperience").append(HTMLworkLocation.replace("%data%", gdk.workLocation));
+$("#workExperience").append(HTMLworkDescription.replace("%data%", gdk.workDescription));
+
+
+
+
+
