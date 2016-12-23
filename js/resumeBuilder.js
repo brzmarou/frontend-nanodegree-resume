3294b2e9b2ba2fea823b41f2e75ae87cdebf728d
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2016-12-23 01:43:42 +0100

    add bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+
+	"name" : "Mariusz Brzostek",
+	"role" : "Web Developer",
+	"contact info" : "a@b.c",
+	"picture URL" : "jakis adres",
+	"welcome massage" : " powitalna wiadomość",
+	"skills": ["programowanie", "Pozycjonowanie stron", "wordpress"]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedContactInfo);
+$("#header").prepend(pictureURL);
+$("#header").prepend(formatedWelcomeMassage);
+$("#header").prepend(formatedSkills);
