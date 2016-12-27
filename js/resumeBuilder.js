43a88ca52302bd355394b6c86ba1812bb15b419d
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2016-12-27 02:12:07 +0100

    ref JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,22 +12,25 @@ var bio = {
 	"skills": ["programowanie", "Pozycjonowanie stron", "wordpress"]
 };
 
-var cen = {
-	"workEmployer" : "Centrum Edukacji Nauczycieli w Gdańsku",
-	"workTitle" : "Specjalista ds. e-learningu",
-	"workDates" : "2014-2015",
-	"workLocation" : "Gdańsk, Polska",
-	"workDescription" : "administracja systemami, tworzenie e-learningu, programowanie"
-}
 
-var gdk = {
-	"workEmployer" : "Gdański Dom Książki w Gdańsku",
-	"workTitle" : "informatyk",
-	"workDates" : "2013",
-	"workLocation" : "Gdańsk, Polska",
-	"workDescription" : "administracja systemami, e-marketing"
+work = {
+	"companies" : [
+		{
+			"workEmployer" : "Centrum Edukacji Nauczycieli w Gdańsku",
+			"workTitle" : "Specjalista ds. e-learningu",
+			"workDates" : "2014-2015",
+			"workLocation" : "Gdańsk, Polska",
+			"workDescription" : "administracja systemami, tworzenie e-learningu, programowanie"
+			},
+			{
+			"workEmployer" : "Gdański Dom Książki w Gdańsku",
+			"workTitle" : "informatyk",
+			"workDates" : "2013",
+			"workLocation" : "Gdańsk, Polska",
+			"workDescription" : "administracja systemami, e-marketing"
+			}
+		]
 }
-
 var education = {
 	"schools":[ 
 		{
@@ -61,10 +64,10 @@ var formattedContactInfo = HTMLemail.replace("%data%", bio.contactInfo);
 var formattedPictureURL = HTMLbioPic.replace("%data%", bio.pictureURL);
 var formattedWelcomeMassage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMassage);
 
-var formattedSkills1 = HTMLskills.replace("%data%", bio.skills[0]);
-var formattedSkills2 = HTMLskills.replace("%data%", bio.skills[1]);
-var formattedSkills3 = HTMLskills.replace("%data%", bio.skills[2]);
-var formattedSkills4 = HTMLskills.replace("%data%", bio.skills[3]);
+var formattedSkillsTemp = [];
+for(var i=0; i<bio.skills.length; i++) {
+	 formattedSkills[i] = HTMLskills.replace("%data%", bio.skills[i]);
+}
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
@@ -72,24 +75,18 @@ $("#footerContacts").prepend(formattedContactInfo);
 $("#header").append(formattedPictureURL);
 $("#header").append(formattedWelcomeMassage);
 $("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills1);
-$("#header").append(formattedSkills2);
-$("#header").append(formattedSkills3);
-$("#header").append(formattedSkills4);
+for(var i=0; i<bio.skills.length; i++) {
+	$("#header").append(formattedSkills[i]);
+}
 
+for(var i=0; i < education.schools.length; i++) {
 $("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(HTMLworkEmployer.replace("%data%", cen.workEmployer));
-$("#workExperience").append(HTMLworkTitle.replace("%data%", cen.workTitle));
-$("#workExperience").append(HTMLworkDates.replace("%data%", cen.workDates));
-$("#workExperience").append(HTMLworkLocation.replace("%data%", cen.workLocation));
-$("#workExperience").append(HTMLworkDescription.replace("%data%", cen.workDescription));
-
-$("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(HTMLworkEmployer.replace("%data%", gdk.workEmployer));
-$("#workExperience").append(HTMLworkTitle.replace("%data%", gdk.workTitle));
-$("#workExperience").append(HTMLworkDates.replace("%data%", gdk.workDates));
-$("#workExperience").append(HTMLworkLocation.replace("%data%", gdk.workLocation));
-$("#workExperience").append(HTMLworkDescription.replace("%data%", gdk.workDescription));
+$("#workExperience").append(HTMLworkEmployer.replace("%data%", work.companies[i].workEmployer));
+$("#workExperience").append(HTMLworkTitle.replace("%data%", work.companies[i].workTitle));
+$("#workExperience").append(HTMLworkDates.replace("%data%", work.companies[i].workDates));
+$("#workExperience").append(HTMLworkLocation.replace("%data%", work.companies[i].workLocation));
+$("#workExperience").append(HTMLworkDescription.replace("%data%", work.companies[i].workDescription));
+}
 
 for(var i=0; i < education.schools.length; i++) {
 $("#education").append(HTMLschoolStart);
